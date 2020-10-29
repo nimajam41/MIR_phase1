d7b00bb3b261e30952068c3f9ac0e39d56639bfd
nima <nimxj4141@gmail.com>
2020-10-29 03:34:15 +0330

    insert completed

--- Phase 1/our_functions.py
+++ Phase 1/our_functions.py
@@ -6,8 +6,10 @@ import matplotlib.pyplot as plt
 from nltk.corpus import stopwords
 import nltk
 
+global stop_words_dic
 
-def prepare_text(documents, lang):
+
+def prepare_text(documents, lang, stop_words):
     if lang == "English":
         processed_documents = []
         all_tokens = []
@@ -21,19 +23,22 @@ def prepare_text(documents, lang):
                 parts += [removed_punctuation_part]
                 all_tokens += [word for word in removed_punctuation_part]
             processed_documents += [parts]
-        frequency_counter = Counter(all_tokens)
-        tokens_size = len(all_tokens)
-        sorted_token_counter = frequency_counter.most_common(len(frequency_counter))
-        sorted_token_ratio = [(c[0], c[1] / tokens_size) for c in sorted_token_counter]
-        stop_words = [sorted_token_counter[i][0] for i in range(40)]
-        remaining_terms = [(sorted_token_counter[i][0], sorted_token_counter[i][1]) for i in
-                           range(40, len(frequency_counter))]
-        r = range(40)
-        y = [sorted_token_counter[i][1] for i in range(40)]
-        plt.bar(r, y, color="red", align="center")
-        plt.title("Stopwords Frequencies")
-        plt.xticks(r, stop_words, rotation="vertical")
-        plt.show()
+        if len(stop_words) == 0:
+            frequency_counter = Counter(all_tokens)
+            tokens_size = len(all_tokens)
+            sorted_token_counter = frequency_counter.most_common(len(frequency_counter))
+            sorted_token_ratio = [(c[0], c[1] / tokens_size) for c in sorted_token_counter]
+            stop_words = [sorted_token_counter[i][0] for i in range(40)]
+            remaining_terms = [(sorted_token_counter[i][0], sorted_token_counter[i][1]) for i in
+                               range(40, len(frequency_counter))]
+            r = range(40)
+            y = [sorted_token_counter[i][1] for i in range(40)]
+            plt.bar(r, y, color="red", align="center")
+            plt.title("Stopwords Frequencies")
+            plt.xticks(r, stop_words, rotation="vertical")
+            plt.show()
+        else:
+            remaining_terms = []
         final_tokens = []
         stemmer = SnowballStemmer(lang.lower())
         for i in range(len(documents)):
@@ -42,11 +47,10 @@ def prepare_text(documents, lang):
                 parts[j] = [word for word in parts[j] if word not in stop_words]
                 parts[j] = [stemmer.stem(word) for word in parts[j]]
                 final_tokens += [word for word in parts[j]]
-        return final_tokens, processed_documents, remaining_terms
+        return final_tokens, processed_documents, remaining_terms, stop_words
 
 
-def positional(input_list):  # input_list is english_structured_documents
-    positional_index_creation = dict()
+def positional(input_list, positional_index_creation, start, end):  # input_list is english_structured_documents
     # positional_index = {
     #   term: {
     #      docID: {     docID is now line in excel file
@@ -55,14 +59,17 @@ def positional(input_list):  # input_list is english_structured_documents
     #   }
     # }
 
-    for docID in range(len(input_list)):
+    for docID in range(start - 1, end):
         for col in range(2):
-            for ind in range(len(input_list[docID][col])):
-                term = input_list[docID][col][ind]
+            for ind in range(len(input_list[docID - start + 1][col])):
+                term = input_list[docID - start + 1][col][ind]
                 if term not in positional_index_creation.keys():  # new term
                     positional_index_creation[term] = dict()
+                    positional_index_creation[term]["cf"] = dict()
+                    positional_index_creation[term]["cf"] = 0
                 if docID not in positional_index_creation[term].keys():  # our term is found in new docID
                     positional_index_creation[term][docID] = dict()
+                positional_index_creation[term]["cf"] += 1
                 if col == 0:
                     if "title" not in positional_index_creation[term][
                         docID].keys():  # term in this title for first time
@@ -80,16 +87,15 @@ def positional(input_list):  # input_list is english_structured_documents
     return positional_index_creation
 
 
-def bigram(input_list):
-    bigram_creation = dict()
+def bigram(input_list, bigram_creation, start, end):
     # bigram = {
     #   sub_term: [terms]
     # }
 
-    for docID in range(len(input_list)):
+    for docID in range(start - 1, end):
         for col in range(2):
-            for ind in range(len(input_list[docID][col])):
-                term = input_list[docID][col][ind]
+            for ind in range(len(input_list[docID - start + 1][col])):
+                term = input_list[docID - start + 1][col][ind]
                 for i in range(0, len(term), 1):
                     if i == 0:
                         sub_term = "$" + term[0]
@@ -105,6 +111,15 @@ def bigram(input_list):
     return bigram_creation
 
 
+def insert(documents, lang, bigram_index, positional_index, docs_size):
+    doc_tokens, docs_structured, doc_terms, doc_stops = prepare_text(documents, lang, stop_words_dic[lang])
+    print(docs_structured, len(documents))
+    bigram_index = bigram(docs_structured, bigram_index, docs_size + 1, docs_size + len(documents))
+    positional_index = positional(docs_structured, positional_index, docs_size + 1, docs_size + len(documents))
+    docs_size += len(documents)
+    return bigram_index, positional_index
+
+
 english_columns = ["description", "title"]
 english_df = pd.read_csv("data/ted_talks.csv", usecols=english_columns)
 x = len(english_df)
@@ -113,9 +128,15 @@ for i in range(x):
     title = english_df.iloc[i]["title"]
     description = english_df.iloc[i]["description"]
     english_documents += [[title, description]]
-english_documents_tokens, english_structured_documents, english_terms = prepare_text(english_documents, "English")
-
-positional_index = positional(english_structured_documents)
-bigram_index = bigram(english_structured_documents)
-print(positional_index["creativ"])
-print(bigram_index["sh"])
+english_documents_tokens, english_structured_documents, english_terms, english_stops = prepare_text(english_documents,
+                                                                                                    "English", [])
+stop_words_dic = {"English": english_stops}
+docs_size = len(english_structured_documents)
+positional_index = dict()
+positional_index = positional(english_structured_documents, positional_index, 1, len(english_structured_documents))
+bigram_index = dict()
+bigram_index = bigram(english_structured_documents, bigram_index, 1, len(english_structured_documents))
+print(positional_index["shadow"])
+bigram_index, positional_index = insert([["hi Mars", "doors and shadows look amazing!"]], "English", bigram_index,
+                                        positional_index, docs_size)
+print(positional_index["shadow"])
