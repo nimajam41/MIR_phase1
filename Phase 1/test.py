f53b8c82efebce50da13f3a3de0fa1b36ef690fb
nima <nimxj4141@gmail.com>
2020-11-05 03:53:36 +0330

    just example

--- Phase 1/test.py
+++ Phase 1/test.py
@@ -1,18 +1,21 @@
 import sys
 
-s = "111010100"
+s = "10111010100"
 x = int(s, 2)
 r = x.to_bytes(len(s) // 8 + 1, sys.byteorder)
 bytes = []
-# f = open("inp", 'rb')
+# f = open("inp", 'wb')
 # f.write(r)
 # f.close()
-f = open("inp", 'wb')
+f = open("inp", 'rb')
 byte = f.read(1)
 while byte:
     bytes += [byte]
     byte = f.read(1)
 f.close()
-print(bytes)
-
-
+bytes.reverse()
+s2 = ""
+for byte in bytes:
+    a = int.from_bytes(byte, sys.byteorder)
+    s2 += format(a, 'b')
+print(s2)
